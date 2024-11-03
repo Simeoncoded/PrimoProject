38ac36e6a7c90d342da539b7a618e72ac23ff180
bluekanary <bluekanary.business@gmail.com>
2024-11-03 14:45:58 -0500

    changed script sources & created script base for login

--- scripts/search.js
+++ scripts/search.js
@@ -21,32 +21,3 @@ function updateSearch(){ // Help from https://www.w3schools.com/howto/howto_js_f
     }
 }
 
-<<<<<<< HEAD
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-=======
->>>>>>> ef15b9ffa7e4df1ba25f8e84a30c67b5893f396a
