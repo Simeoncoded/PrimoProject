6b3b5b8e85b7130c981c8b875edadd857d10102f
bluekanary <bluekanary.business@gmail.com>
2024-11-04 15:24:51 -0500

    Changed login page to select a user role.

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,21 @@
 
+loggedUser = {role:""}
+
 function selectUser(){
 
-    return 0;
+    radSelectedUser = document.querySelector('input[name="radUsers"]:checked').value
+    for (role in userRoles){
+        if (radSelectedUser == role){
+            loggedUser.role = role;
+        }
+    }
+    
+    window.location.href = "index.html"
+
+}
+
+const userRoles = {
+    admin:"Admin",
+    quality:"Quality",
+    engineer:"Engineer"
 }
