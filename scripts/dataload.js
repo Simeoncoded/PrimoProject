a66c3672b5f050849f489969de292b580ce7010f
bluekanary <bluekanary.business@gmail.com>
2024-11-17 22:08:53 -0500

    edit page

--- scripts/dataload.js
+++ scripts/dataload.js
@@ -1,253 +1,100 @@
 ncr = [
-
     {
-        "ncrNum": "2023-001",
-        "ncrDate": "2023-09-01",
-        "ncrStatus": "open",
+    "ncrNum":"2023-001",
+    "ncrDate":"2023-09-09",
+    "ncrStatus":"open",
 
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
+    "identifyProcess":{
+        "recInspect":false,
+        "prodOrder":true
         },
-        "supplierName": "John Wick",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
+    "supplierName":"Johnwick",
+    "prodNum":"1984-A",
+    "saleOrderNum":"21",
+    "itemDescription":"jimminy crickets",
+    "defectDescription":"Somebody's poisoned the waterhole",
+    "quantityRecieved":"200",
+    "quantityDefective":"150",
 
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
+    "qaName":"Deer Man",
+    "nonConforming":true,
+    "needEngineer":true
     },
 
     {
-        "ncrNum": "2022-002",
-        "ncrDate": "2022-10-09",
-        "ncrStatus": "open",
+    "ncrNum":"2022-001",
+    "ncrDate":"2022-09-09",
+    "ncrStatus":"open",
 
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
+    "identifyProcess":{
+        "recInspect":false,
+        "prodOrder":true
         },
-        "supplierName": "Cody Gaps",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
+    "supplierName":"Johnwick",
+    "prodNum":"1984-A",
+    "saleOrderNum":"21",
+    "itemDescription":"jimminy crickets",
+    "defectDescription":"Somebody's poisoned the waterhole",
+    "quantityRecieved":"200",
+    "quantityDefective":"150",
 
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
+    "qaName":"Deer Man",
+    "nonConforming":true,
+    "needEngineer":true
     },
-
+    
     {
-        "ncrNum": "2022-003",
-        "ncrDate": "2022-02-03",
-        "ncrStatus": "open",
+    "ncrNum":"2022-002",
+    "ncrDate":"2022-02-03",
+    "ncrStatus":"open",
 
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
+    "identifyProcess":{
+        "recInspect":false,
+        "prodOrder":true
         },
-        "supplierName": "Wick wade",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
+    "supplierName":"Johnwick",
+    "prodNum":"1984-A",
+    "saleOrderNum":"21",
+    "itemDescription":"jimminy crickets",
+    "defectDescription":"Somebody's poisoned the waterhole",
+    "quantityRecieved":"200",
+    "quantityDefective":"150",
 
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-004",
-        "ncrDate": "2022-02-05",
-        "ncrStatus": "closed",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "Drink Water",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-005",
-        "ncrDate": "2022-02-05",
-        "ncrStatus": "closed",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "John Cal",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-006",
-        "ncrDate": "2022-02-05",
-        "ncrStatus": "closed",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "Tom Tom",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-007",
-        "ncrDate": "2022-02-05",
-        "ncrStatus": "open",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "John Bones",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-008",
-        "ncrDate": "2022-05-05",
-        "ncrStatus": "open",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "Aspinall Tom",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-009",
-        "ncrDate": "2022-02-05",
-        "ncrStatus": "closed",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "Gonya Dan",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-010",
-        "ncrDate": "2022-02-10",
-        "ncrStatus": "closed",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "Dan wells",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
-    },
-    {
-        "ncrNum": "2022-011",
-        "ncrDate": "2022-02-09",
-        "ncrStatus": "open",
-
-        "identifyProcess": {
-            "recInspect": false,
-            "prodOrder": true
-        },
-        "supplierName": "Jake Paul",
-        "prodNum": "1984-A",
-        "saleOrderNum": "21",
-        "itemDescription": "jimminy crickets",
-        "defectDescription": "Somebody's poisoned the waterhole",
-        "quantityRecieved": "200",
-        "quantityDefective": "150",
-
-        "qaName": "Deer Man",
-        "nonConforming": true,
-        "needEngineer": true
+    "qaName":"Deer Man",
+    "nonConforming":true,
+    "needEngineer":true
     }
-
 ]
 
 table = document.querySelector("#ncrlog")
 
 ncr.forEach(form => {
-
-    table.insertAdjacentHTML("afterbegin",
+        
+    table.insertAdjacentHTML("afterbegin", 
         '<tr>' +
-        '<td>' + form.ncrNum + '</td>' +
-        '<td>' + form.ncrDate + '</td>' +
-        '<td>' + form.supplierName + '</td>' +
-        '<td class="status-' + form.ncrStatus + '">' + form.ncrStatus + '</td>' +
-        '<td>   <button class="vbtn" onclick="showNcrEdit(' + form + ')">Edit</button>' +               // Add function to accept json object
-        '       <button class="vbtn" onclick="showNcrDetails(' + form + ')">Details</button></td>' +    // change function to accept json object
+            `<td>${form.ncrNum}</td>` +
+            `<td>${form.ncrDate}</td>` +
+            `<td>${form.supplierName}</td>` +
+            `<td class="status-${form.ncrStatus}">${form.ncrStatus}</td>` +
+            `<td>   <button class="vbtn" onclick="showNcrEdit('${form.ncrNum}')">Edit</button>` +               // Add function to accept json object
+            `       <button class="vbtn" onclick="showNcrDetails('${form.ncrNum}')">Details</button></td>` +    // change function to accept json object
         '</tr>')                                                                                            // showNcrEdit and showNcrDetails should link to an edit/details page and filled by json object
-
+       
 });
+
+function showNcrEdit(ncrnum){
+
+    document.location.href = "edit.html";
+    // grab ids from edit.html here
+
+    ncr.forEach(form => {   
+        if (form.ncrNum == ncrnum){
+            console.log(form.ncrNum)
+
+            // Fill edit.html with data from form
+
+        }
+
+    })
+
+}
