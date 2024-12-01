82d06bc4cb94b4b587bc29b43e17c6a1c97f43e4
bluekanary <bluekanary.business@gmail.com>
2024-11-30 22:19:19 -0500

    up

--- scripts/dataload.js
+++ scripts/dataload.js
@@ -1,38 +1,38 @@
 ncrs = [
     {
-    date:"2024-11-25",
-    defect_description:"The eggs are white and yellow",
-    ennotneeded:"Yes",
-    item_description:"Green eggs and Ham",
-    ncr_no:"2024-001",
-    nonconforming:"Yes",
-    po_prod_no:"1278",
-    process:"Supplier or Rec-Insp",
-    quality_rep_name:"Doctor Seuss",
-    quantity_defective:"200",
-    quantity_received:"2000",
-    sales_order_no:"433",
-    status:"open",
-    supplier_name:"Green Goods Ltd.",
+        date: "2024-11-25",
+        defect_description: "The eggs are white and yellow",
+        ennotneeded: "Yes",
+        item_description: "Green eggs and Ham",
+        ncr_no: "2024-001",
+        nonconforming: "Yes",
+        po_prod_no: "1278",
+        process: "Supplier or Rec-Insp",
+        quality_rep_name: "Doctor Seuss",
+        quantity_defective: "200",
+        quantity_received: "2000",
+        sales_order_no: "433",
+        status: "open",
+        supplier_name: "Green Goods Ltd.",
     }
 ]
 
 suppliers = [
-        {name:"Acme Supplies"},
-        {name:"Green Goods Ltd."},
-        {name:"Fast Track Supplies"},
-        {name:"Prime Sources Inc."},
-        {name:"Blue Ocean Trading"},
-        {name:"World Wide Distributors"},
-        {name:"Eco Trade Partners"},
-        {name:"Luxury Goods International"},
-        {name:"Mega Supplies Co."},
-        {name:"Top Choice Suppliers"}
+    "Acme Supplies",
+    "Green Goods Ltd.",
+    "Fast Track Supplies",
+    "Prime Sources Inc.",
+    "Blue Ocean Trading",
+    "World Wide Distributors",
+    "Eco Trade Partners",
+    "Luxury Goods International",
+    "Mega Supplies Co.",
+    "Top Choice Suppliers"
 ]
 
 function callCurrentUser() {
 
-    //localStorage.getItem('user')
+    console.log(localStorage.getItem('user'))
 
 }
 
@@ -41,9 +41,12 @@ function codeAddress() {
     if (existingNCRs == -1) {
         localStorage.setItem('ncrs', JSON.stringify(ncrs))
         localStorage.setItem('ncrLastNum', ncrs.length)
-        location.reload()
-        
     }
+    const existingSuppliers = JSON.parse(localStorage.getItem('suppliers')) || -1;
+    if (existingNCRs == -1) {
+        localStorage.setItem('suppliers', JSON.stringify(suppliers))
+    }
+    location.reload()
 }
 
 window.onload = codeAddress;
