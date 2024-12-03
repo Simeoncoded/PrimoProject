8838bd24022f9eb7c60281bbc79974d0d82e1564
Simeoncoded <simeonolawore07@gmail.com>
2024-12-03 17:27:51 -0500

    new changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,28 @@
+mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize notification and email counters
     const notificationCounter = document.getElementById('notification-counter');
@@ -125,6 +150,9 @@ document.querySelector('#btnSave').addEventListener('click', function(event) {
     localStorage.setItem('ncrFormData', JSON.stringify(formData));
     alert('Form data saved successfully!');
 });
+
+
+
 
 
 
